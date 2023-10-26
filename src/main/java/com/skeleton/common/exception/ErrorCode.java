7039b0ceb02896b6da5533e32651ab6dfa08fd0c
Hyeri Jang / 장혜리 <46921979+hyerijang@users.noreply.github.com>
2023-10-26 13:02:41 +0900

    refactor: 게시물 조회 오류  상태 코드 수정

--- src/main/java/com/skeleton/common/exception/ErrorCode.java
+++ src/main/java/com/skeleton/common/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     // 예시)
     // RECRUITMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "R001", "해당하는 채용공고가 없습니다."),
-    POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "P001", "해당하는 게시물이 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "해당하는 게시물이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
