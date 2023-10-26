d8011197364e34df9215956e730433c3da6a0b7f
jeongjiwon <jungkt0x@naver.com>
2023-10-26 20:35:41 +0900

    feat : 비밀번호 암호화 구현
    scrypt를 이용하여 회원가입시 입력한 비밀번호를 암호화하여 DB에 저장합니다.

--- src/main/java/com/skeleton/user/service/UserService.java
+++ src/main/java/com/skeleton/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.skeleton.user.service;
 
 import com.skeleton.common.exception.CustomException;
 import com.skeleton.common.exception.ErrorCode;
+import com.skeleton.user.crypto.PasswordEncoder;
 import com.skeleton.user.dto.UserSignupRequest;
 import com.skeleton.user.dto.UserSignupResponse;
 import com.skeleton.user.entity.User;
@@ -17,16 +18,19 @@ import java.util.Random;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserSignupResponse saveUser(UserSignupRequest request) {
 
         checkDuplicateEmail(request);
 
+        String encryptedPassword = passwordEncoder.encrypt(request.getPassword());
+
         User user = User.builder()
                 .account(request.getAccount())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(encryptedPassword)
                 .build();
 
         userRepository.save(user);
