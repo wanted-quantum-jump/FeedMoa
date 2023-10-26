7489d09b0e7e645f8aae58ec90f11a15c9f5f2ae
BA <windy01999@gmail.com>
2023-10-26 14:38:26 +0900

    feat: 스프링 시큐리티 초기 설정 추가

---
+++ src/main/java/com/skeleton/auth/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.skeleton.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers("*").permitAll()
+        );
+
+        http.csrf(CsrfConfigurer::disable);
+
+        http.sessionManagement(httpSecuritySessionManagementConfigurer ->
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )
+        );
+
+        return http.build();
+    }
+}
