312bb63558aab24a28fe73312a9444286ad22e82
jeongjiwon <jungkt0x@naver.com>
2023-10-26 20:16:51 +0900

    refactor : api 변경
    /feedmoa/join -> /api/signup 으로 변경하였습니다.

--- src/main/java/com/skeleton/user/controller/UserController.java
+++ src/main/java/com/skeleton/user/controller/UserController.java
@@ -9,16 +9,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/feedmoa/join")
+    @PostMapping("/signup")
     public ResponseEntity<?> joinUser(@Valid @RequestBody UserSignupRequest request) {
 
         UserSignupResponse verifyCode = userService.saveUser(request);
