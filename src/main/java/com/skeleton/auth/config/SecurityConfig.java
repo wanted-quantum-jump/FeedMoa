393c481caebde6f7a72d05007ac816a4f77b194a
BA <windy01999@gmail.com>
2023-10-28 12:29:14 +0900

    feat: auth 컨트롤러 구현, 스프링 시큐리티 설정

--- src/main/java/com/skeleton/auth/config/SecurityConfig.java
+++ src/main/java/com/skeleton/auth/config/SecurityConfig.java
@@ -1,22 +1,45 @@
 package com.skeleton.auth.config;
 
+import com.skeleton.auth.filter.JwtAuthenticationFilter;
+import com.skeleton.auth.filter.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Lazy
+    @Autowired
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
+                          JwtAuthorizationFilter jwtAuthorizationFilter){
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // 회원가입 말고는 모두 인증/인가가 필요하다.
         http.authorizeHttpRequests(request -> request
-                .anyRequest().permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/signup").permitAll()
+                .anyRequest().authenticated()
         );
 
         http.csrf(CsrfConfigurer::disable);
@@ -27,5 +50,10 @@ public class SecurityConfig {
         );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
